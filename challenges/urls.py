6ac6cb104ceb1424320cd7fc32147bedbafdd182
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-08 23:05:15 +0530

    feat (challenges app): Add new route for numbers routing

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("<month>",views.monthly)
+    path("<int:month>",views.monthly_by_number),
+    path("<str:month>",views.monthly_by_name)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,48 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound
 
-def january(request):
-    return HttpResponse("This works January!!!")
 
-def february(request):
-    return HttpResponse("This works February!!!")
-
-def march(request):
-    return HttpResponse("This works March!!!")
-
-def april(request):
-    return HttpResponse("This works April!!!")
-
-def may(request):
-    return HttpResponse("This works May!!!")
-
-def june(request):
-    return HttpResponse("This works June!!!")
-
-def july(request):
-    return HttpResponse("This works July!!!")
-
-def august(request):
-    return HttpResponse("This works August!!!")
-
-def september(request):
-    return HttpResponse("This works September!!!")
-
-def october(request):
-    return HttpResponse("This works October!!!")
-
-def november(request):
-    return HttpResponse("This works November!!!")
-
-def december(request):
-    return HttpResponse("This works December!!!")
-
-def monthly(request, month): # django will pass the same parameter, if use month in urls, then month will be passed in views
-    
-    challenge_text= ""+ month
+def monthly_by_name(request, month): # django will pass the same parameter, if use month in urls, then month will be passed in views
     
     months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 
               'august', 'september', 'october', 'november', 'december']
     if month.lower() not in months:
         return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse("This month of "+challenge_text +" works for me ")
+    
+    return HttpResponse("This month of "+month +" works for me ")
+
+def monthly_by_number(request,month):
+
+    if (month>12 or month<1):
+        return HttpResponseNotFound("This month is not supported")
+    else:
+        months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 
+              'august', 'september', 'october', 'november', 'december']
+        return monthly_by_name(request,months[month-1])     
