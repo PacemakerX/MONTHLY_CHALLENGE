e311a2dce22e8572e4c1cad233fb7eb284bd9b60
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-08 22:39:28 +0530

    feat (challenges app): Add challenge for each month

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("This works!!!")
+def january(request):
+    return HttpResponse("This works January!!!")
+
+def february(request):
+    return HttpResponse("This works February!!!")
+
+def march(request):
+    return HttpResponse("This works March!!!")
+
+def april(request):
+    return HttpResponse("This works April!!!")
+
+def may(request):
+    return HttpResponse("This works May!!!")
+
+def june(request):
+    return HttpResponse("This works June!!!")
+
+def july(request):
+    return HttpResponse("This works July!!!")
+
+def august(request):
+    return HttpResponse("This works August!!!")
+
+def september(request):
+    return HttpResponse("This works September!!!")
+
+def october(request):
+    return HttpResponse("This works October!!!")
+
+def november(request):
+    return HttpResponse("This works November!!!")
+
+def december(request):
+    return HttpResponse("This works December!!!")
