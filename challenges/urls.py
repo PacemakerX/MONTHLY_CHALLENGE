4c7a7a5a4cab5ce98a7f5cc543bd7a98b411deb6
Sparsh Soni <sparsh.officialwork@gmail.com>
2025-05-09 14:59:03 +0530

    fix (name): Give a base page a name

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
     path("<int:month>",views.monthly_by_number),
     path("<str:month>",views.monthly_by_name,name="month-challenge"), # giving a name to the path
-    path("",views.monthly_rdirect)
+    path("",views.monthly_rdirect,name="index")
 ]
